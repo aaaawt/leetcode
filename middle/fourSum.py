7d87ee9f05a54d5858577e7fefa61bef7947532e
aaaawt <weiting54@outlook.com>
2022-02-08 03:01:56 +0800

    foursum

---
+++ middle/fourSum.py
@@ -0,0 +1,5 @@
+def fourSum(nums, target):
+    pass
+
+print(fourSum([1, 0, -1, 0, -2, 2], 0))
+print(fourSum([2, 2, 2, 2, 2], 8))
--- middle/letterCombinations.py
+++ middle/letterCombinations.py
@@ -1,6 +1,17 @@
 def letterCombinations(digits):
-    pass
+    if digits == '':
+        return []
+    nums = ['abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+    ll = ['']
+    for x in digits:
+        res = []
+        for j in ll:
+            for i in nums[int(x) - 2]:
+                res.append(j + i)
+        ll = res
+    return ll
 
+print(letterCombinations('3456'))
 print(letterCombinations('23'))
 print(letterCombinations(''))
 print(letterCombinations('2'))
