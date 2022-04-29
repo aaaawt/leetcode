20b5aa2193473e164f0287643c7929da06084e38
aaaawt <weiting54@outlook.com>
2022-04-29 23:22:11 +0800

    copyRandomList

--- offer1/2_35.py
+++ offer1/2_35.py
@@ -6,5 +6,24 @@ class Node:
 
 
 class Solution:
-    def copyRandomList(self, head: 'Node') -> 'Node':
-        pass
+    def copyRandomList(self, head: 'Node'):
+        if not head:
+            return None
+        node = head
+        while node:
+            newnode = Node(node.val)
+            newnode.next = node.next
+            node.next = newnode
+            node = node.next.next
+        node = head
+        while node:
+            newnode = node.next
+            newnode.random = node.random.next if node.random is not None else None
+            node = node.next.next
+        node = head
+        newhead = head.next
+        while node:
+            newnode = node.next
+            node = newnode.next
+            newnode.next = newnode.next.next if newnode.next is not None else None
+        return newhead
